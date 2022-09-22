93ddab6a3245eef54b17826a607015507ab75ddb
tp <61617660+ittp@users.noreply.github.com>
2022-09-22 14:58:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./styles.css";
 
 import { Panel, PanelGroup, Placeholder, InputPicker } from "rsuite";
 
-const App = () => (
-  <PanelGroup accordion defaultActiveKey={1} bordered>
-    <Panel header="Производитель" eventKey={1} id="panel1">
-      <Placeholder.Paragraph />
-      <InputPicker />
-    </Panel>
-    <Panel header="Panel 2" eventKey={2} id="panel2">
-      <Placeholder.Paragraph />
-    </Panel>
-    <Panel header="Panel 3" eventKey={3} id="panel3">
-      <Placeholder.Paragraph />
+
+const ViewPage = (content) => (
+  
+  let config = { 
+  group: {
+    accordion: true, 
+    defaultActiveKey:1, 
+    bordered: true 
+  
+  }
+  }
+  
+  <PanelGroup {...group} >
+    <Panel header="Title" eventKey={1} id="panel1">
+      {content}
     </Panel>
   </PanelGroup>
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//ReactDOM.render(<App />, document.getElementById("root"));
